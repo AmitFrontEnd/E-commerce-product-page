13f5d6b736daf8cd758950f2dc0a11f644979df5
Amit Kumar <amitemailu@gmail.com>
2025-03-11 19:04:58 +0530

    cart logic done

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const menuIcon=document.querySelector('.menu-icon')
 const closeNav=document.querySelector('.close-icon')
 const nav=document.querySelector('nav')
+const cart=document.querySelector('.cart')
+const cartContainer=document.querySelector('.cart-container')
 
 menuIcon.addEventListener('click',()=>{
     nav.classList.add('active')
 })
 closeNav.addEventListener('click',()=>{
     nav.classList.remove('active')
+})
+
+cart.addEventListener('click',()=>{
+    cartContainer.classList.toggle('active')
 })
